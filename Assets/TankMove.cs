29d91b0929aab4e64ee48b144a4fe85387bda315
Dongkyoo <wind.dong.dream@gmail.com>
2019-07-19 00:54:30 +0900

    Tank 움직임 구현

---
+++ Assets/TankMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMove : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * 5.0f * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * 5.0f * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * 5.0f * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * 5.0f * Time.deltaTime);
+        }
+    }
+    
+    
+}
